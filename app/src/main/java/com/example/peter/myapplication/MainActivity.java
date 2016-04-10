de4e23e0ca9e3a22ec585d473c7c88f64b70eb62
Peter Huang <Peter760504@gmail.com>
2016-04-10 12:43:49 +0800

    <新增>Item Swipe 功能，把target list讀出並以Adapter顯示

--- app/src/main/java/com/example/peter/myapplication/MainActivity.java
+++ app/src/main/java/com/example/peter/myapplication/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.peter.myapplication;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.daimajia.swipe.SwipeLayout;
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnMenuTabClickListener;
+import com.daimajia.swipe.util.Attributes;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 
 public class MainActivity extends Activity {
 
@@ -25,79 +25,140 @@ public class MainActivity extends Activity {
 
     private ListView rewardListView;
     private List<TargetEntity> RewardList;
-    private TargetAdapter targetAdapter;
+    private TargetSwipeAdapter targetSwipeAdapter;
     private TargetEntity selectTargetEntity;
+    private Context mContext = this;
+    private ArraySwipeAdapter arraySwipeAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.target_list_layout);
 
+        rewardListView = (ListView) findViewById(R.id.goodTargetListView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            if (actionBar != null) {
+                actionBar.setTitle("ListView");
+            }
+        }
+
         targetDAO = new TargetDAO(getApplicationContext());
         RewardList = getRewardList();
-        int layoutId = R.layout.swipe_layout;
-        targetAdapter = new TargetAdapter(getApplicationContext(), layoutId, RewardList);
-
-        sample1 = (SwipeLayout) findViewById(R.id.sample1);
-        sample1.setShowMode(SwipeLayout.ShowMode.PullOut);
-        View starBottView = sample1.findViewById(R.id.starbott);
-        sample1.addDrag(SwipeLayout.DragEdge.Left, sample1.findViewById(R.id.bottom_wrapper));
-        sample1.addDrag(SwipeLayout.DragEdge.Right, sample1.findViewById(R.id.bottom_wrapper_2));
-        sample1.addDrag(SwipeLayout.DragEdge.Top, starBottView);
-        sample1.addDrag(SwipeLayout.DragEdge.Bottom, starBottView);
-        sample1.addRevealListener(R.id.delete, new SwipeLayout.OnRevealListener() {
+        targetSwipeAdapter = new TargetSwipeAdapter(this,RewardList);
+        rewardListView.setAdapter(targetSwipeAdapter);
+        targetSwipeAdapter.setMode(Attributes.Mode.Single);
+        rewardListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onReveal(View child, SwipeLayout.DragEdge edge, float fraction, int distance) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ((SwipeLayout)(rewardListView.getChildAt(position - rewardListView.getFirstVisiblePosition()))).open(true);
             }
         });
-
-        sample1.getSurfaceView().setOnClickListener(new View.OnClickListener() {
+        rewardListView.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Click on surface", Toast.LENGTH_SHORT).show();
-//                Log.d(MyActivity.class.getName(), "click on surface");
+            public boolean onTouch(View v, MotionEvent event) {
+//                Log.e("ListView", "OnTouch");
+                return false;
             }
         });
-        sample1.getSurfaceView().setOnLongClickListener(new View.OnLongClickListener() {
+        rewardListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(MainActivity.this, "longClick on surface", Toast.LENGTH_SHORT).show();
-//                Log.d(MyActivity.class.getName(), "longClick on surface");
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(mContext, "OnItemLongClickListener", Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
-        sample1.findViewById(R.id.star2).setOnClickListener(new View.OnClickListener() {
+        rewardListView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Star", Toast.LENGTH_SHORT).show();
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+//                Log.e("ListView", "onScrollStateChanged");
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
             }
         });
 
-        sample1.findViewById(R.id.trash2).setOnClickListener(new View.OnClickListener() {
+        rewardListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Trash Bin", Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Log.e("ListView", "onItemSelected:" + position);
             }
-        });
 
-        sample1.findViewById(R.id.magnifier2).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Magnifier", Toast.LENGTH_SHORT).show();
+            public void onNothingSelected(AdapterView<?> parent) {
+//                Log.e("ListView", "onNothingSelected:");
             }
         });
+        //
 
-        sample1.addRevealListener(R.id.starbott, new SwipeLayout.OnRevealListener() {
-            @Override
-            public void onReveal(View child, SwipeLayout.DragEdge edge, float fraction, int distance) {
-                View star = child.findViewById(R.id.star);
-                float d = child.getHeight() / 2 - star.getHeight() / 2;
-//                ViewHelper.setTranslationY(star, d * fraction);
-//                ViewHelper.setScaleX(star, fraction + 0.6f);
-//                ViewHelper.setScaleY(star, fraction + 0.6f);
-            }
-        });
+//        targetDAO = new TargetDAO(getApplicationContext());
+//        RewardList = getRewardList();
+//        int layoutId = R.layout.swipe_layout;
+//        arraySwipeAdapter = new ArraySwipeAdapter(getApplicationContext(), layoutId, RewardList);
+//
+//        sample1 = (SwipeLayout) findViewById(R.id.sample1);
+//        sample1.setShowMode(SwipeLayout.ShowMode.PullOut);
+//        View starBottView = sample1.findViewById(R.id.starbott);
+//        sample1.addDrag(SwipeLayout.DragEdge.Left, sample1.findViewById(R.id.bottom_wrapper));
+//        sample1.addDrag(SwipeLayout.DragEdge.Right, sample1.findViewById(R.id.bottom_wrapper_2));
+//        sample1.addDrag(SwipeLayout.DragEdge.Top, starBottView);
+//        sample1.addDrag(SwipeLayout.DragEdge.Bottom, starBottView);
+//        sample1.addRevealListener(R.id.delete, new SwipeLayout.OnRevealListener() {
+//            @Override
+//            public void onReveal(View child, SwipeLayout.DragEdge edge, float fraction, int distance) {
+//
+//            }
+//        });
+//
+//        sample1.getSurfaceView().setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Click on surface", Toast.LENGTH_SHORT).show();
+////                Log.d(MyActivity.class.getName(), "click on surface");
+//            }
+//        });
+//        sample1.getSurfaceView().setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                Toast.makeText(MainActivity.this, "longClick on surface", Toast.LENGTH_SHORT).show();
+////                Log.d(MyActivity.class.getName(), "longClick on surface");
+//                return true;
+//            }
+//        });
+//        sample1.findViewById(R.id.star2).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Star", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        sample1.findViewById(R.id.trash2).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Trash Bin", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        sample1.findViewById(R.id.magnifier2).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Magnifier", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        sample1.addRevealListener(R.id.starbott, new SwipeLayout.OnRevealListener() {
+//            @Override
+//            public void onReveal(View child, SwipeLayout.DragEdge edge, float fraction, int distance) {
+//                View star = child.findViewById(R.id.star);
+//                float d = child.getHeight() / 2 - star.getHeight() / 2;
+////                ViewHelper.setTranslationY(star, d * fraction);
+////                ViewHelper.setScaleX(star, fraction + 0.6f);
+////                ViewHelper.setScaleY(star, fraction + 0.6f);
+//            }
+//        });
 
     }
 
--- app/src/main/java/com/example/peter/myapplication/TargetSwipeAdapter.java
+++ app/src/main/java/com/example/peter/myapplication/TargetSwipeAdapter.java
@@ -4,83 +4,83 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.daimajia.swipe.adapters.ArraySwipeAdapter;
+import com.daimajia.swipe.SimpleSwipeListener;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.BaseSwipeAdapter;
 
 import java.util.List;
 
-/**
- * Created by peter on 2016/3/29.
- */
-public class TargetSwipeAdapter extends ArraySwipeAdapter<TargetEntity> {
 
-    // 畫面資源編號
-    private int resource;
-    // 包裝的記事資料
+public class TargetSwipeAdapter extends BaseSwipeAdapter {
+
+    private Context mContext;
+    private TargetEntity selectTargetEntity;
     private List<TargetEntity> targetEntityList;
+    private TargetDAO targetDAO;
 
-    public TargetSwipeAdapter(Context context, int resource, List<TargetEntity> targetEntityList) {
-        super(context, resource, targetEntityList);
-        this.resource = resource;
+    public TargetSwipeAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public TargetSwipeAdapter(Context mContext, List<TargetEntity> targetEntityList) {
+        this.mContext = mContext;
         this.targetEntityList = targetEntityList;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LinearLayout itemView;
-        // 讀取目前位置的記事物件
-        final TargetEntity targetEntity = (TargetEntity) getItem(position);
-
-        if (convertView == null) {
-            // 建立項目畫面元件
-            itemView = new LinearLayout(getContext());
-            String inflater = Context.LAYOUT_INFLATER_SERVICE;
-            LayoutInflater li = (LayoutInflater)
-                    getContext().getSystemService(inflater);
-            li.inflate(resource, itemView, true);
-        }
-        else {
-            itemView = (LinearLayout) convertView;
-        }
-
-        // 讀取記事顏色、已選擇、標題與日期時間元件
-//        RelativeLayout typeColor = (RelativeLayout) itemView.findViewById(R.id.type_color);
-//        ImageView selectedItem = (ImageView) itemView.findViewById(R.id.selected_item);
-        TextView targetView = (TextView) itemView.findViewById(R.id.SwipeItemTextView);
-//        TextView pointView = (TextView) itemView.findViewById(R.id.point_text);
-
-        // 設定記事顏色
-//        GradientDrawable background = (GradientDrawable)typeColor.getBackground();
-//        background.setColor(item.getColor().parseColor());
-
-        // 設定標題與日期時間
-        targetView.setText("名稱：" + targetEntity.getTargetName() + "  -  分數：" + String.valueOf(targetEntity.getPoint()));
-//        pointView.setText("分數：" + String.valueOf(targetEntity.getPoint()));
-
-        // 設定是否已選擇
-//        selectedItem.setVisibility(item.isSelected() ? View.VISIBLE : View.INVISIBLE);
-
-        return itemView;
-    }
-
-    // 設定指定編號的記事資料
-    public void set(int index, TargetEntity targetEntity) {
-        if (index >= 0 && index < targetEntityList.size()) {
-            targetEntityList.set(index, targetEntity);
-            notifyDataSetChanged();
-        }
-    }
-
-    // 讀取指定編號的記事資料
-    public TargetEntity get(int index) {
-        return targetEntityList.get(index);
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
+//        return 0;
     }
 
     @Override
-    public int getSwipeLayoutResourceId(int position) {
-        return 0;
+    public View generateView(int position, ViewGroup parent) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.swipe_layout, null);
+        SwipeLayout swipeLayout = (SwipeLayout) v.findViewById(getSwipeLayoutResourceId(position));
+        swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+            @Override
+            public void onOpen(SwipeLayout layout) {
+//                YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
+            }
+        });
+        swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
+            @Override
+            public void onDoubleClick(SwipeLayout layout, boolean surface) {
+                Toast.makeText(mContext, "DoubleClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+        v.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "click delete", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void fillValues(int position, View convertView) {
+        TargetEntity targetEntity = targetEntityList.get(position);
+        TextView swipeItemTextView = (TextView) convertView.findViewById(R.id.SwipeItemTextView);
+//        t.setText((position + 1) + ".");
+        swipeItemTextView.setText("名稱：" + targetEntity.getTargetName() + " - 點數：" + String.valueOf(targetEntity.getPoint()));
+    }
+
+    @Override
+    public int getCount() {
+        return targetEntityList.size();
+    }
+
+    @Override
+    public TargetEntity getItem(int position) {
+        return targetEntityList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 }
