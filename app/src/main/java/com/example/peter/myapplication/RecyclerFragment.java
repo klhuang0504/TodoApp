8b977cad4d6bcb0310068865b114019186aaa276
Peter Huang <peter760504@gmail.com>
2016-05-04 17:43:13 +0800

    <測試>RecyclerView練習 Part2

--- app/src/main/java/com/example/peter/myapplication/RecyclerFragment.java
+++ app/src/main/java/com/example/peter/myapplication/RecyclerFragment.java
@@ -1,55 +1,16 @@
 package com.example.peter.myapplication;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.util.Attributes;
-import com.example.peter.myapplication.FileUtil;
-import com.example.peter.myapplication.R;
-import com.example.peter.myapplication.data.LogDAO;
-import com.example.peter.myapplication.data.LogEntity;
-import com.example.peter.myapplication.data.TargetDAO;
-import com.example.peter.myapplication.data.TargetEntity;
-import com.example.peter.myapplication.data.UserDAO;
-import com.example.peter.myapplication.data.UserEntity;
-import com.example.peter.myapplication.target.TargetSwipeAdapter;
-import com.example.peter.myapplication.target.TargetSwipeAdapterCallback;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
-import static android.graphics.Color.WHITE;
-import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 
 /**
  * Created by peter on 2016/3/29.
@@ -69,12 +30,13 @@ public class RecyclerFragment extends Fragment {
         for(int i = 0; i < 100; i++){
             myDataset.add(i + "");
         }
-        MyAdapter myAdapter = new MyAdapter(myDataset);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(myDataset, getContext());
+        recyclerViewAdapter.setMode(Attributes.Mode.Single);
         RecyclerView mList = (RecyclerView) view.findViewById(R.id.targetRecyclerView);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mList.setLayoutManager(layoutManager);
-        mList.setAdapter(myAdapter);
+        mList.setAdapter(recyclerViewAdapter);
         return view;
     }
 
---
+++ app/src/main/java/com/example/peter/myapplication/RecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package com.example.peter.myapplication;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.swipe.SimpleSwipeListener;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+
+import java.util.List;
+
+/**
+ * Created by peter on 2016/5/3.
+ */
+public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapter.SimpleViewHolder> {
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+        SwipeLayout swipeLayout;
+        TextView mTextView;
+
+        public SimpleViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.SwipeItemTextView);
+            swipeLayout = (SwipeLayout) v.findViewById(R.id.singleRecycleLinearLayout);
+        }
+    }
+
+    private List<String> mData;
+    private Context mContext;
+
+    public RecyclerViewAdapter(List<String> data, Context context) {
+        this.mData = data;
+        this.mContext = context;
+    }
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_recyler_view_layout, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+//    @Override
+//    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View v = LayoutInflater.from(parent.getContext())
+//                .inflate(R.layout.single_recyler_view_layout, parent, false);
+//        return new ViewHolder(v);
+//    }
+
+    @Override
+    public void onBindViewHolder(final SimpleViewHolder viewHolder, final int position) {
+        viewHolder.mTextView.setText(mData.get(position));
+        viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+        viewHolder.swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+            @Override
+            public void onOpen(SwipeLayout layout) {
+//                YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
+                Toast.makeText(mContext, "OnOpen!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+//        mItemManger.bindView(viewHolder.v ,position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.singleRecycleLinearLayout;
+    }
+}
